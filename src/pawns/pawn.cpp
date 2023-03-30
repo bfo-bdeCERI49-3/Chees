1ed875fb31f6c31641420a34ab8cbea03f210b42
bilaliscarioth <bilal@iscarioth.org>
2023-03-30 15:45:30 +0200

    Adding pawns header, move IPiece

---
+++ src/pawns/pawn.cpp
@@ -0,0 +1,6 @@
+#include "pawns/pawn.hpp"
+
+
+Pawn::Pawn() {
+    
+}
---
+++ src/pawns/rook.hpp
@@ -0,0 +1,9 @@
+#pragma once 
+
+class Rook : Piece {
+    public:
+        inline bool isTaken() { return this->taken; };      // Check if the piece was taken or not 
+        inline void taken() { this->taken = true; };        // Set the piece as taken
+        
+        int move();                                         // To override in each piece class
+}
