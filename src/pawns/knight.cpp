6d5d0c5b4dbdf91b953ed849104db84712eac975
DarsFabian <dars.fabian@gmail.com>
2023-03-31 04:24:21 +0200

    Knight movements

--- src/pawns/knight.cpp
+++ src/pawns/knight.cpp
@@ -2,35 +2,72 @@
 #include "board/board.h"
 #include <iostream>
 
-Knight::Knight ( Color set_color, size_t set_position ) {
+Knight::Knight(Color set_color, size_t set_position)
+{
     color = set_color;
     position = set_position;
 }
 
-void Knight::checkMoves(Board& m_board) {
+void Knight::checkMoves(Board &m_board)
+{
     int allow_i = 0;
-    for ( int i = 0; i < 8; i++ ) {
+    for (int i = 0; i < 8; i++)
+    {
         int next_pos = position + allowedMoves[i];
-        if (next_pos >= 0 && next_pos <= 63 ) {
-            if ( (next_pos + 1) % 8 == 0 || next_pos % 8 == 0) {
+        if (next_pos >= 0 && next_pos <= 63)
+        {
+            if ((next_pos + 1) % 8 == 0 || next_pos % 8 == 0)
+            {
                 continue;
             }
 
-            if ( m_board.cases[next_pos].type == NOTHING ) {
-                possibleMoves[allow_i] = next_pos;
-                allow_i++;
-            } else {
-                //Eat.
+            if (this->color == White)
+            {
+                // Si la case est vide
+                if (m_board.cases[next_pos].type == NOTHING)
+                {
+                    possibleMoves[allow_i] = next_pos;
+                    allow_i++;
+                }
+                else
+                {
+                    if (m_board.cases[next_pos].type != NOTHING && m_board.cases[next_pos].m_pawn->getColor() == Black)
+                    {
+                        possibleMoves[allow_i] = next_pos;
+                        allow_i++;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                // Si la case est vide
+                if (m_board.cases[next_pos].type == NOTHING)
+                {
+                    possibleMoves[allow_i] = next_pos;
+                    allow_i++;
+                }
+                else
+                {
+                    if (m_board.cases[next_pos].type != NOTHING && m_board.cases[next_pos].m_pawn->getColor() == White)
+                    {
+                        possibleMoves[allow_i] = next_pos;
+                        allow_i++;
+                        break;
+                    }
+                }
             }
         }
     }
 
-    for(int i = 0; i < 8; i++){
+    for (int i = 0; i < 8; i++)
+    {
         std::cout << possibleMoves[i] << ";";
     }
     std::cout << "\n";
 }
 
-int Knight::move(Board& m_board) {
+int Knight::move(Board &m_board)
+{
     checkMoves(m_board);
 }
